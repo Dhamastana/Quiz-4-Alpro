92ba09f0e65c19d33219eec94b55d8c4991daf4d
Dhamastana <54705729+Dhamastana@users.noreply.github.com>
2019-09-11 21:55:33 -0700

    Add files via upload

---
+++ Quiz41_DhamastanaDD_1201180226.py
@@ -0,0 +1,6 @@
+listmahasiswa = ["Anton", "Budi", "Doni", "Huda"]
+print("List Mahasiswa : ")
+print(listmahasiswa[0])
+print(listmahasiswa[1])
+print(listmahasiswa[2])
+print(listmahasiswa[3])
---
+++ Quiz42_DhamastanaDD_1201180226.py
@@ -0,0 +1,9 @@
+fruits = ["Nanas", "Apel", "Pepaya", "Kentang"]
+length = len(fruits)
+i = 0
+
+print("Buah-buahan : ")
+while i < length:
+    print(fruits[i])
+    i = i + 1
+
